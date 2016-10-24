7eb4bf4de70f8e4c4571d8460a02808f0f3ad893
hjt <1085660049@qq.com>
2016-10-24 15:03:40 +0800

    get node brother & parent

--- legoc/control/node.py
+++ legoc/control/node.py
@@ -13,3 +13,29 @@ def child(id):
         })
     return children
 
+
+def brother(id):
+    src_node = LGNode.objects.filter(pk=id)
+    src_brothers = NodeRef.objects.filter(node_left=src_node, ref=1)
+    brothers = []
+    for src_brother in src_brothers:
+        brothers.append({
+            'id': src_brother.id,
+            'name': src_brother.node_right.name,
+            'desp': src_brother.node_right.desp
+        })
+    return brothers
+
+
+def parent(id):
+    src_node = LGNode.objects.filter(pk=id)
+    src_parents = NodeRef.objects.filter(node_left=src_node, ref=0)
+    parents = []
+    for src_parent in src_parents:
+        parents.append({
+            'id': src_parent.id,
+            'name': src_parent.node_right.name,
+            'desp': src_parent.node_right.desp
+        })
+    return parents
+
--- legoc/views.py
+++ legoc/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here
 from django.views.decorators.csrf import csrf_exempt
 
-from legoc.control.node import child
+from legoc.control.node import child, brother, parent
 from legoc.control.login import LoginCtrl
 from legoc.control.logout import LogoutCtrl
 from legoc.control.register import RegisterCtrl
@@ -21,6 +21,29 @@ def node_child(request):
         return HttpResponse(json_helper.dumps_err(0, node_list))
     else:
         return HttpResponse(json_helper.dump_err_msg(-1, '返回错误'))
+
+
+@csrf_exempt
+def node_brother(request):
+    """获取用例兄弟结点信息，默认传入的都是左结点"""
+    node_id = request.GET['Node_id']
+    node_list = brother(node_id)
+    if node_list is not None:
+        return HttpResponse(json_helper.dumps_err(0, node_list))
+    else:
+        return HttpResponse(json_helper.dump_err_msg(-1, '返回错误'))
+
+
+@csrf_exempt
+def node_parent(request):
+    """获取用例父母结点信息"""
+    node_id = request.GET['Node_id']
+    node_list = parent(node_id)
+    if node_list is not None:
+        return HttpResponse(json_helper.dumps_err(0, node_list))
+    else:
+        return HttpResponse(json_helper.dump_err_msg(-1, '返回错误'))
+
 
 
 def login(request):
